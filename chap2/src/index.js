320d9ff6014703fb0690e89d5e952c500b8469a1
lolimay <1404363070@qq.com>
2018-09-01 01:06:17 +0800

    fix npm install problem (never use cnpm again)

--- chap2/src/index.js
+++ chap2/src/index.js
@@ -1,1 +1,13 @@
-document.writeln('Hello wepack!')
+const h1 = document.createElement('h1')
+const ul = document.createElement('ul')
+const urls = ['./assets/images/cat.png', './assets/images/dog.png']
+
+h1.innerText = 'Snapshots'
+urls.forEach((url) => {
+    const li = document.createElement('li')
+    li.innerHTML = `<a href="${url}">11</a>`
+    ul.appendChild(li)
+})
+
+document.body.appendChild(h1)
+document.body.appendChild(ul)
